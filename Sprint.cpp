14d310aa3e23c542956ac443eea3a75786d8afed
Doug Turnbull <dturnbull@o19s.com>
2012-12-30 22:47:39 -0500

    Switch to the Ape way of doing things

--- Sprint.cpp
+++ Sprint.cpp
@@ -5,8 +5,30 @@
 #include <iostream>
 #include <stdint.h>
 #include "PerfTimer.h"
+#include <string>
+#include "Ape.h"
+#include "SprintBin.h"
 
-#include "format_x.h"
+class Sprint2
+{
+	uint32_t val;
+public:
+	Sprint2(uint32_t valToFormat) : val(valToFormat) {}
+
+	void AppendTo(std::string& str)
+	{
+		char asStr[9];
+		_itoa_s(val, &asStr[0], 9, 16); 
+		str.append(asStr, 9);
+	}
+};
+
+std::string operator + (std::string&& lhs, Sprint2&& rhs)
+{
+	rhs.AppendTo(lhs);
+	return std::move(lhs);
+
+}
 
 
 int main(int argc, char* argv[])
@@ -17,7 +39,7 @@ int main(int argc, char* argv[])
 	PerfTimer timer;
 	for (unsigned int i = 0xf; i < 100; ++i)
 	{
-		Sprint<sprint::flags::NoFlags, -1, -1, 'x', uint32_t>::append(dest, 60, i);
+		Ape(dest2, 60) + "Hello " + SpBin< Power<4> >(i);
 	}
 	uint64_t elapsed = timer.Stop();
 	std::cout << "Doug:" << dest << " Time: " << elapsed <<  std::endl;
@@ -25,11 +47,21 @@ int main(int argc, char* argv[])
 	PerfTimer timer2;
 	for (unsigned int i = 0xf; i < 100; ++i)
 	{
-		sprintf_s(dest2, 60, "%x", i);
+		sprintf_s(dest2, 60, "Hello %x", i);
 	}
 	elapsed = timer2.Stop();
 
 	std::cout << "SprintF:" << dest2 << " Time: " << elapsed <<  std::endl;
+
+	std::string foo = "";
+	PerfTimer timer3;
+	for (unsigned int i = 0xf; i < 100; ++i)
+	{
+		foo = std::string() + Sprint2(i);
+	}
+	elapsed = timer3.Stop();
+
+	std::cout << "Sprint2" << foo << " Time: " << elapsed << std::endl;	
 
 }
 
--- all_statics.cpp
+++ all_statics.cpp
@@ -1,14 +1,10 @@
 #include "stdafx.h"
-#include "format_base.h"
-#include "format_x.h"
+#include "SprintBin.h"
 
 #define SPRINT_COMPILETIME_INTTOSTR(i) #i	
 
 namespace sprint
 {
-
-	char Sprint<sprint::flags::NoFlags, -1, -1, 'x', uint32_t>::lookup[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8',
-	'9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
 
 }
